0b92d19e6ece421f1737e297d3646c00797c1094
lincolnneiva64 <lincolnneiva64@gmail.com>
2026-03-25 14:52:40 -0300

    corrige rota principal de core para estoque

--- sistema/urls.py
+++ sistema/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', include('core.urls')),
+    path('', include('estoque.urls')),
 
     path('admin/', admin.site.urls),
     path('estoque/', include('estoque.urls')),
