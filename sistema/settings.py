64d745413993460a2b2c555cd4048bf2cef5165e
lincolnneiva64 <lincolnneiva64@gmail.com>
2026-05-02 05:40:23 -0300

    Prepara configuracao minima para deploy

--- sistema/settings.py
+++ sistema/settings.py
@@ -20,8 +20,9 @@ from decouple import config, Csv
 import os
 
 SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = str(config('DEBUG', default='False')).strip().lower() in {'1', 'true', 'yes', 'on', 'debug'}
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='', cast=Csv())
+CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', default='', cast=Csv())
 CHECKLIST_BASE_URL = config("CHECKLIST_BASE_URL", default=os.getenv("CHECKLIST_BASE_URL", "")).rstrip("/")
 
 # Application definition
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -76,7 +78,9 @@ WSGI_APPLICATION = 'sistema.wsgi.application'
 import dj_database_url
 
 DATABASES = {
-    'default': dj_database_url.parse(config('DATABASE_URL'))
+    'default': dj_database_url.parse(
+        config('DATABASE_URL', default=f'sqlite:///{BASE_DIR / "db.sqlite3"}')
+    )
 }
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
@@ -113,9 +117,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+STORAGES = {
+    'default': {
+        'BACKEND': 'django.core.files.storage.FileSystemStorage',
+    },
+    'staticfiles': {
+        'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+    },
+}
 
 if not DEBUG:
     SECURE_SSL_REDIRECT = True
